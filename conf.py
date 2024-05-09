e9d6ece5d22c47a9522813207bd32597634fba5d
ITSAIDI <noureddinesaidi111@gmail.com>
2024-05-09 16:37:07 +0100

    m

--- conf.py
+++ conf.py
@@ -13,15 +13,11 @@ author = 'GIIA'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    #[...]
-    "sphinxcontrib.video",
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-video_enforce_extra_source = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
